23204c1a207ede0e3f6b6455d4340cbb90301475
Claude_Delcroix <166008956+Yurri5code@users.noreply.github.com>
2024-11-07 01:37:25 +0300

    Create Least Common Multiple, and Highest Common.c

---
+++ Least Common Multiple, and Highest Common.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+main ( )
+{
+int x/y'/a/b;
+printf ("Enter two numbers
+scanf ("%d %d" I &x , &y) ;
+a=x;b=y;
+while(a!=b)
+{
+if (a<b)
+a=a+x;
+else
+b=b+y;
+}
+printf("LCM of' %d and %d is %d\n",x,Y,a);
+a=x; b=y;
+while(a!=b)
+{
+if(a>b)
+a=a-b;
+else
+b=b-a;
+}
+printf ("HCF of %d and %d is. %d\n", x, y, a) ;
+}
