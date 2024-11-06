3a24d96a6b56ddc82c7f6561ddd47db80602fd52
Claude_Delcroix <166008956+Yurri5code@users.noreply.github.com>
2024-11-07 00:16:13 +0300

    Create russianPeasant.c

---
+++ russianPeasant.c
@@ -0,0 +1,24 @@
+:include<stdio.h>
+:nain ( )
+{
+int a,b,x,y,s:O;
+printf ("Enter two numbers to be multiplied ") ;
+scanf("%d%dH,&X,&y) ;
+a:x;
+b=y;
+while(a>:l)
+{
+if(a%2!:O)
+s:s+b;
+a/:2;
+b*:2;
+}
+print'f ("%d * %d
+Problem 4
+I*Loop till first number reduces to 1*1
+1*1f first number is odd*1
+1 *Add second number to s * 1
+I*Divide first number by 2*1
+I*Multiply Second number by two*1
+%d\n H,X,y, s);
+}
